0ed9ab0360b2b8f7f8d01ec1cfc2a3cf69122d1f
tejas0123 <tejasrc27@gmail.com>
2023-02-27 11:33:48 +0530

    added navbar component

---
+++ src/module1.mjs
@@ -0,0 +1,7 @@
+const father = "Ravi";
+const mother = "Rajeshwari";
+const myName = "Tejas";
+const pet = "dog";
+
+export default myName;
+export {father, mother, pet};
---
+++ src/module2.mjs
@@ -0,0 +1,3 @@
+import name, {father, mother, pet} from './module1.mjs'
+console.log(name);
+console.log(father, mother, pet);
